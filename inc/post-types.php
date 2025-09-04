d063e27420ca4baa8d6704d63b10b2dd20ae4abb
muhamedsakr07 <muhamedsakr07@gmail.com>
2025-09-04 12:39:32 +0300

    Resolve merge conflicts

--- inc/post-types.php
+++ inc/post-types.php
@@ -1,6 +1,7 @@
 <?php
 
 if ( ! defined( 'ABSPATH' ) ) exit;
+
 //here i managed the post type for scalability and increasing for post types
 
 include_once plugin_dir_path(__FILE__) . '/post-types/ai-community.php';
